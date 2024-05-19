09c41c486a23dd3dd23e48d59ad8c82630fc674c
ZaneMikula <120997447+mikuza32@users.noreply.github.com>
2024-05-19 16:44:52 -0400

    Added the main class and fibonacciSequence class
    - Main class of the fibonacciSequence program has been added
    - main class executes the arithmetic from the fibonacciSequence method
    - fibonacciSequence class and method set the arithmetic for the entire program

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Zane Mikula
+// 4802C-31959
+// May 19th, 2024
+// Demonstrates on how to develop a recursive static method that returns the nth term in the
+// Fibonacci Sequence
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 10; // sets the object n as an integer equal to 10
+        int result = fibonacciSequence.fibonacci(n); // sets the result object as an integer, which is the final product after fibonacci method
+        System.out.println(" The " + n + "th numeral of the fibonacci sequence is " + result ); // print out similar to a toString that prints the results, that are not hard coded as instructed
+
+    }
+}
---
+++ fibonacciSequence.java
@@ -0,0 +1,9 @@
+public class fibonacciSequence {
+
+    public static int fibonacci(int n) { // fibonacci method is used to manipulate the int n as instructed
+        if (n <= 1) { // if n is less than or equal to 1, than the console will return 10. Since n = 10 in the main method
+            return n; // returns n to the console
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2); // if n is greater than 1, execute and return n(n-1) + n(n-2)
+    }
+}
