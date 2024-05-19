85959a4315bcf55ce055da2a0cbff33d121bbbbe
ZaneMikula <120997447+mikuza32@users.noreply.github.com>
2024-05-19 17:30:00 -0400

    Update fibonacciSequence.java
    - Added necessary Javadoc comments following industry Standard procedure

--- fibonacciSequence.java
+++ fibonacciSequence.java
@@ -1,5 +1,9 @@
 public class fibonacciSequence {
-
+ /**
+     * Calculates the nth numeral of the fibonacciSequence
+     * @param n the numeral to calculate
+     * @return value of the n numeral
+     */
     public static int fibonacci(int n) { // fibonacci method is used to manipulate the int n as instructed
         if (n <= 1) { // if n is less than or equal to 1, than the console will return 10. Since n = 10 in the main method
             return n; // returns n to the console
